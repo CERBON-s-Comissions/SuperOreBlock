5c58ec068b86ab1506d2ccaf44deecf1ffcae0f7
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-06-08 20:50:56 -0300

    Initial commit

---
+++ Forge/src/main/java/com/cerbon/ml_template/forge/ModNameForge.java
@@ -0,0 +1,12 @@
+package com.cerbon.ml_template.forge;
+
+import com.cerbon.ml_template.ModName;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ModName.MOD_ID)
+public class ModNameForge {
+
+    public ModNameForge() {
+        ModName.init();
+    }
+}
